4f608cdc82750004f4ed6c9de1dc9551f134ffdf
abhilash <kasarla.abhilash@gmail.com>
2020-05-28 17:06:36 -0700

    initial commit, base files

---
+++ flask_api.py
@@ -0,0 +1,15 @@
+"""
+
+@author: Abhilash Kasarla
+"""
+
+from flask import Flask, request
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+
+
+if __name__=='__main__':
+	app.run()
